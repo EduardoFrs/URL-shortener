02b7514f23eb8840b06120d0b160044e7e8f6918
EduardoFrs <fernandeseduardo00@gmail.com>
2025-02-13 16:21:03 +0100

    Ajout des fichiers main, requirement et README

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# URL-shortener
+
+# URL Shortener API
+
+Application allowing you to generate shortened URL, and to count their number of visits.
+
+Made with Python, using FastAPI framework.
+
+## Get Ready
+
+Requirements :
+   - Python
+   - pip
+
+1. Clone the project repository
+```bash
+  git clone https://github.com/EduardoFrs/URL-shortener.git
+  cd URL-shortener
+```
+2. Install dependencies
+```bash
+  pin install requirements.txt
+```
+3. Run the app
+```bash
+  uvicorn main:app --reload
+```
+The API will be available at http://127.0.0.1:8000
+
+## Future Improvements
+
+- Add database for store the URL and number of clicks
+- Fix the counter problem
+
+## Authors
+
+- Eduardo RIBEIRO FERNANDES
+- Github : [@EduardoFrs](https://github.com/EduardoFrs)
---
+++ main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, HTTPException, Response
+from pydantic import BaseModel
+import validators, secrets
+
+app = FastAPI()
+
+# stockage pour les urls raccourcis et le compteur de clic
+urls = {}
+clicks = {}
+
+# modèle pour verifier avec pydantic les url rentrés
+class URLBase(BaseModel):
+    target_url: str
+
+class URLShort(URLBase):
+    short_url: str
+    clicks: int
+
+@app.get("/")
+def read_root():
+    return {"Hello, you are on URL Shortener API"}
+
+# route pour raccourcir l'URL
+@app.post("/shorten", response_model=URLShort)
+def shorten_url(url: URLBase):
+    if not validators.url(url.target_url):
+        raise HTTPException(status_code=400, detail="Provided URL is not valid")
+
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmonpqrstuvwxyz0123456789"
+    short_key = "".join(secrets.choice(chars) for _ in range(6))
+    urls[short_key] = url.target_url
+    clicks[short_key] = 0
+
+    return URLShort(target_url=url.target_url, short_url=f"http://127.0.0.1:8000/{short_key}", clicks=0)
+
+# route pour rediriger vers l'URL de base via le raccourci
+@app.get("/{short_key}")
+def redirect_url(short_key: str, response: Response):
+    long_url = urls.get(short_key)
+    if not long_url:
+        raise HTTPException(status_code=404, detail="URL not found")
+        clicks[short_key] = clicks.get(short_key, 0) + 1
+    response.status_code = 307
+    response.headers["Location"] = long_url
+    return response
+
+# route pour le compteru de clic
+@app.get("/{short_key}/stats")
+def url_stats(short_key: str):
+    if short_key not in clicks:
+        raise HTTPException(status_code=404, detail="URL not found")
+
+    return {"short_url": f"http://127.0.0.1:8000/{short_key}", "clicks": clicks[short_key]}
